ff6c021edf482ac806b7301185a03a768261754a
Ubuntu <ubuntu@ubuntu-14.04-amd64.general.kilo.mtl.linux.novalocal>
2017-02-23 20:25:47 +0000

    removed unused scripts

--- rbenv.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-exec $SHELL
--- ruby-build.sh
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
-exec $SHELL
